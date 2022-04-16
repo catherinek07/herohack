8a93c3c7a4d82fcb2aca7dac3992079073915d5c
Catherine Kim <80084168+catherinek07@users.noreply.github.com>
2022-04-16 15:54:29 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,9 @@ import Grid from './Grid'
 import Footer from './Footer'
 import './Home.css';
 //changes to imports 
-import SecurityIcon from '@material-ui/icons/Security';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import TrendingUpIcon from '@material-ui/icons/TrendingUp';
-import ImportExportIcon from '@material-ui/icons/ImportExport';
-import ComputerIcon from '@material-ui/icons/Computer';
-import HttpIcon from '@material-ui/icons/Http';
-
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import SearchIcon from '@material-ui/icons/Search';
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -23,7 +19,7 @@ const theme = createMuiTheme({
   },
   typography: {
     fontFamily: [
-      'Roboto'
+      'Source Sans Pro'
     ],
     h4: {
       fontWeight: 600,
@@ -73,14 +69,9 @@ function Home() {
           </Typography>
         </div>
         <div className={`${classes.grid} ${classes.bigSpace}`}>
-          <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Secure" btnTitle="Show me More" />
-          <Grid icon={<EventNoteIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Reliable" btnTitle="Show me More"/>
-          <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Performant" btnTitle="Show me More"/>
-        </div>
-        <div className={`${classes.grid} ${classes.littleSpace}`}>  
-          <Grid icon={<ImportExportIcon style={{fill: "#5EA780", height:"125", width:"125"}}/>}  title="Modular" btnTitle="Show me More"/>
-          <Grid icon={<ComputerIcon style={{fill: "#E69426", height:"125", width:"125"}}/>}  title="Multi-Platform" btnTitle="Show me More"/>
-          <Grid icon={<HttpIcon style={{fill: "#2EA09D", height:"125", width:"125"}}/>} title="Connected" btnTitle="Show me More"/>
+          <Grid icon={<ShoppingCartIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Buy" btnTitle="Let's Go" />
+          <Grid icon={<AttachMoneyIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Sell" btnTitle="Let's Go"/>
+          <Grid icon={<SearchIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Explore" btnTitle="Let's Go"/>
         </div>
         <div className={classes.bigSpace}>
           <Footer/>
