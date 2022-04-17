900a69ac33ee124eb1739203911546e345c25c4e
Catherine Kim <80084168+catherinek07@users.noreply.github.com>
2022-04-16 23:07:44 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import {BottomNavigation, BottomNavigationAction} from '@material-ui/core'
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import YouTubeIcon from '@material-ui/icons/YouTube';
+
+import {Typography} from '@material-ui/core'
 
 function Footer() {
     return (
-        <BottomNavigation >
-          <BottomNavigationAction color="red" label="Facebook" value="recents" icon={<FacebookIcon  style={{fill: "#3b5998"}}/>} />
-          <BottomNavigationAction label="Twitter" value="favorites" icon={<TwitterIcon  style={{fill: "#1DA1F2"}}/>} />
-          <BottomNavigationAction label="Instagram" value="nearby" icon={<InstagramIcon  style={{fill: " #C13584"}}/>} />
-          <BottomNavigationAction label="YouTube" value="folder" icon={<YouTubeIcon  style={{fill: "#c4302b"}}/>} />
-        </BottomNavigation>
+      <Typography>
+        iNFiniTe is brought to you by Frank Li, Caleb Ji, and Catherine Kim
+      </Typography>
 )
 }
 
